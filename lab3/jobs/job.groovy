81051db7c039789a6de530f190f1f68ac5f324e3
oleg-lukianov <oleg.lukianov8@gmail.com>
2020-10-02 15:45:35 +0300

    Mod job 2

--- lab3/jobs/job.groovy
+++ lab3/jobs/job.groovy
@@ -1,13 +1,4 @@
 pipelineJob("lab3-build-docker-image") {
-        logRotator {
-            numToKeep(10)
-        }
-
-    parameters {
-      choiceParam('ACTION', ["plan", "apply"], "Choose current action")
-      stringParam('COMMIT_HASH')
-        booleanParam('BUILD_IMAGE', true, 'Build image')
-    }
 
     definition {
         cpsScm {
