7ccd83058dbc743de279b82359d8465f974b3e2c
oleg-lukianov <oleg.lukianov8@gmail.com>
2020-10-02 15:12:37 +0300

    Create Jenkinsfile

--- lab3/jobs/job.groovy
+++ lab3/jobs/job.groovy
@@ -1,21 +1,21 @@
-pipelineJob("test-pipeline_original") {
-    	logRotator {
-       		numToKeep(10)
-    	}
+pipelineJob("build-docker-image") {
+        logRotator {
+            numToKeep(10)
+        }
 
     parameters {
       choiceParam('ACTION', ["plan", "apply"], "Choose current action")
       stringParam('COMMIT_HASH')
-	    booleanParam('BUILD_IMAGE', true, 'Build image')
+        booleanParam('BUILD_IMAGE', true, 'Build image')
     }
 
     definition {
         cpsScm {
-   			scm {
-            	git {
+            scm {
+                git {
                   remote {
                         github("oleg-lukianov/labs", 'https')
-                        branch("devops")
+                        branch("pull-request")
                   }
               }
         }
@@ -25,3 +25,4 @@ pipelineJob("test-pipeline_original") {
     }
 
  }
+
